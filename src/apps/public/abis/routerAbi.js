3b4a8196e44f1459315be5526f047d24c233dd68
NguyenHuynh <nguyen07t1@gmail.com>
2022-02-03 21:13:07 +0700

    Swap and liquidiy

--- src/apps/public/abis/routerAbi.js
+++ src/apps/public/abis/routerAbi.js
@@ -764,8 +764,3 @@ export const routerAbi = [
     "type": "receive"
   }
 ];
-
-
-// export {
-//     routerAbi
-// }
--- src/apps/public/js/main.js
+++ src/apps/public/js/main.js
@@ -12,8 +12,8 @@ import {erc20Abi} from '../abis/erc20Abi.js';
 import {routerAbi} from '../abis/routerAbi.js';
 
 let swapPair = {
-    "from": { "address": null, "balance": 0, "decimals": 0, "allowance": 0 },
-    "to": { "address": null, "balance": 0, "decimals": 0, "allowance": 0 }
+    "from": { "address": null, "balance": 0, "decimals": 0, "allowance": 0, "swapBalance": '' },
+    "to": { "address": null, "balance": 0, "decimals": 0, "allowance": 0, "swapBalance": '' }
 }
 
 let liquidityPair = {
@@ -71,7 +71,6 @@ async function init() {
     await updateListAvailableTokens(getListTokens());    
     web3 = new Web3(window.ethereum);
     await window.ethereum.enable();
-    console.log(web3);
 }
 
 async function updateListAvailableTokens(tokens) {
@@ -109,18 +108,22 @@ async function selectToken(address) {
         swapPair.from.address = address;
         swapPair.from.balance = readableBalance;
         swapPair.from.allowance = allowance;
+        swapPair.from.decimals = decimals;
     } else if (tokenSelection == 'to') {
         swapPair.to.address = address;
         swapPair.to.balance = readableBalance;
         swapPair.to.allowance = allowance;
+        swapPair.to.decimals = decimals;
     } else if (tokenSelection == 'liquidity_from') {
         liquidityPair.from.address = address;
         liquidityPair.from.balance = readableBalance;
         liquidityPair.from.allowance = allowance;
+        liquidityPair.from.decimals = decimals;
     } else {
         liquidityPair.to.address = address;
         liquidityPair.to.balance = readableBalance;
         liquidityPair.to.allowance = allowance;
+        liquidityPair.to.decimals = decimals;
     }
     updateUI();
 }
@@ -129,26 +132,24 @@ function updateUI() {
     if (!!swapPair.from.address) {
         document.getElementById("from_token_icon").src = tokens[swapPair.from.address].logoURI;
         document.getElementById("from_token_text").innerHTML = tokens[swapPair.from.address].symbol;
-        document.getElementById("from_token_amount").value = swapPair.from.balance;
+        document.getElementById("from_token_amount").value = swapPair.from.swapBalance;
     }
 
     if (!!swapPair.to.address) {
         document.getElementById("to_token_icon").src = tokens[swapPair.to.address].logoURI;
         document.getElementById("to_token_text").innerHTML = tokens[swapPair.to.address].symbol;
-        document.getElementById("to_token_amount").value = swapPair.to.balance;
+        document.getElementById("to_token_amount").value = swapPair.to.swapBalance;
     }
 
     if (!!liquidityPair.from.address) {
         document.getElementById("liquidity_from_token_icon").src = tokens[liquidityPair.from.address].logoURI;
         document.getElementById("liquidity_from_token_text").innerHTML = tokens[liquidityPair.from.address].symbol;
-        document.getElementById("liquidity_from_token_amount").value = liquidityPair.from.balance;
         updateApprovalButtonForAddLiquidity();
     }
 
     if (!!liquidityPair.to.address) {
         document.getElementById("liquidity_to_token_icon").src = tokens[liquidityPair.to.address].logoURI;
         document.getElementById("liquidity_to_token_text").innerHTML = tokens[liquidityPair.to.address].symbol;
-        document.getElementById("liquidity_to_token_amount").value = liquidityPair.to.balance;
         updateApprovalButtonForAddLiquidity();
     }
 }
@@ -176,15 +177,53 @@ function closeModal() {
     document.getElementById("token_modal").style.display = "none";
 }
 
-async function getQuote() {
-    // let amount = Number(Moralis.Units.ETH(document.getElementById('from_token_amount').value));
-    // const quote = await Moralis.Plugins.oneInch.quote({
-    //     chain: 'mumbai',
-    //     fromTokenAddress: swapPair.from.address,
-    //     toTokenAddress: swapPair.to.address,
-    //     amount: amount
-    // });
-    // document.getElementById("to_token_amount").value = quote.toTokenAmount / 10 ** quote.toToken.decimals;
+async function getQuoteSwap(evt) {
+    if (!swapPair.from.address || !swapPair.to.address) {
+        return;
+    }
+    swapPair.from.swapBalance = '';
+    swapPair.to.swapBalance = '';
+
+    const id = evt.srcElement.id;
+    const amount = $('#' + id).val();
+    
+    let etherAmount = amount;
+    
+    let pair = []
+    
+    let token = swapPair.to;
+    if (id == 'from_token_amount') {
+        token = swapPair.from;
+        pair = [swapPair.from.address, swapPair.to.address];
+        // swapPair.from.swapBalance = etherAmount;
+    } else {
+        pair = [swapPair.to.address, swapPair.from.address];
+    }
+    etherAmount = web3.utils.toWei(etherAmount, getEtherUnit(web3.utils.unitMap, token.decimals));
+    
+    const contract = new web3.eth.Contract(routerAbi, mumbaiQuickSwapRouterContract);
+    const result = await contract.methods.getAmountsOut(new web3.utils.BN(etherAmount), pair).call();
+
+    if (id == 'from_token_amount') {
+        swapPair.from.swapBalance = web3.utils.fromWei(etherAmount, getEtherUnit(web3.utils.unitMap, swapPair.from.decimals));
+        swapPair.to.swapBalance = web3.utils.fromWei(result[1], getEtherUnit(web3.utils.unitMap, swapPair.to.decimals));
+    } else {
+        swapPair.from.swapBalance = web3.utils.fromWei(result[1], getEtherUnit(web3.utils.unitMap, swapPair.from.decimals));
+        swapPair.to.swapBalance = web3.utils.fromWei(etherAmount, getEtherUnit(web3.utils.unitMap, swapPair.to.decimals));
+    }
+    updateUI();
+}
+
+function getEtherUnit(unitMap, decimals) {
+    let zeroSum = '1';
+    for (let i = 0; i < decimals; i++) {
+        zeroSum += '0'
+    }
+    const keys = Object.keys(unitMap).filter(k => unitMap[k] == zeroSum)
+    if (keys.length > 0) {
+        return keys[0];
+    }
+    return 'ether';
 }
 
 async function processCustomToken() {
@@ -196,53 +235,84 @@ async function processCustomToken() {
     const symbol = await contract.methods.symbol().call();
     addCustomToken(add, symbol, decimals, '');
     $("#custom_token_address").val("");
-    // const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    // const account = accounts[0];
-    
-    // await contract.methods.approve(mumbaiQuickSwapRouterContract, web3.utils.toWei('10000000000000000000', 'ether')).send({ from: owner });
   }
+}
+
+async function submitAddLiquidity() {
+    const router = new web3.eth.Contract(routerAbi, mumbaiQuickSwapRouterContract);
+    const dataOption = {
+        from: owner
+    }
+    const deadline = web3.utils.toHex(Math.round(Date.now()/1000)+60*20);
+    const unitMap = web3.utils.unitMap;
+    // await router.methods.addLiquidityETH('0xf45b409a2b978ec02Bb6084e6Acc42867a78Ee9c', '100000000000000000000000', '10', '1', owner, deadline).send(dataOption);
+    
+    await router.methods.addLiquidity(
+        liquidityPair.from.address,
+        liquidityPair.to.address,
+        new web3.utils.toWei($('#liquidity_from_token_amount').val(), getEtherUnit(unitMap, liquidityPair.from.decimals)),
+        new web3.utils.toWei($('#liquidity_to_token_amount').val(), getEtherUnit(unitMap, liquidityPair.to.decimals)),
+        '1',
+        '1',
+        owner, deadline).send(dataOption);
+}
+
+async function submitAddLiquidityEth() {
+    const router = new web3.eth.Contract(routerAbi, mumbaiQuickSwapRouterContract);
+    const dataOption = {
+        from: owner,
+        value: new web3.utils.toWei($('#liquidity_from_token_amount').val(), getEtherUnit(unitMap, liquidityPair.from.decimals))
+    }
+    const deadline = web3.utils.toHex(Math.round(Date.now()/1000)+60*20);
+    const unitMap = web3.utils.unitMap;
+    await router.methods.addLiquidityETH('0xf45b409a2b978ec02Bb6084e6Acc42867a78Ee9c', '100000000000000000000000', '10', '1', owner, deadline).send(dataOption);
+}
+
+async function submitApproveToken() {
+    let address = $('#liquidity_approve_token').data('address');
+    contract = new web3.eth.Contract(erc20Abi, address);
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    
+    await contract.methods.approve(mumbaiQuickSwapRouterContract, web3.utils.toWei('10000000000000000000', 'ether')).send({ from: account });
+    $('#liquidity_approve_token').hide();
+}
+
+async function submitSwap() {
+    if (swapPair.from.swapBalance == '') {
+        return;
+    }
+    const router = new web3.eth.Contract(routerAbi, mumbaiQuickSwapRouterContract);
+    const dataOption = {
+        from: owner
+    }
+    const deadline = web3.utils.toHex(Math.round(Date.now()/1000)+60*20);
+    const unitMap = web3.utils.unitMap;
+    await router.methods.swapExactTokensForTokens(
+        web3.utils.toWei(swapPair.from.swapBalance, getEtherUnit(unitMap, swapPair.from.decimals)),
+        '1',
+        [swapPair.from.address, swapPair.to.address],
+        owner, 
+        deadline).send(dataOption);
 }
 
 $(document).ready(() => {
     init();
     document.getElementById("from_token_select").onclick = (() => { openModal('from'); });
     document.getElementById("to_token_select").onclick = (() => { openModal('to'); });
-    document.getElementById("liquidity_approve_token").onclick = (async () => {
-        let address = $('#liquidity_approve_token').data('address');
-        console.log(address);
-        // console.log(web3);
-        contract = new web3.eth.Contract(erc20Abi, address);
-        //web3.utils.toWei(new web3.utils.BN(10 ** 12), 'ether')
-        console.log(owner);
-        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-        const account = accounts[0];
-        
-        await contract.methods.approve(mumbaiQuickSwapRouterContract, web3.utils.toWei('10000000000000000000', 'ether')).send({ from: account });
-        $('#liquidity_approve_token').hide();
-    });
-    document.getElementById("submit_add_liquidity").onclick = (async () => {
-        const gasPrice = await web3.eth.getGasPrice();
-        const router = new web3.eth.Contract(routerAbi, mumbaiQuickSwapRouterContract);
-        const dataOption = {
-            from: owner,
-            value: web3.utils.toWei('0.1')
-        }
-        new web3.utils.BN()
-        console.log(owner);
-        const deadline = web3.utils.toHex(Math.round(Date.now()/1000)+60*20);
-        await router.methods.addLiquidityETH('0xf45b409a2b978ec02Bb6084e6Acc42867a78Ee9c', '100000000000000000000000', '10', '1', owner, deadline).send(dataOption);
-        // await router.methods.addLiquidityETH(mumbaiQuickSwapRouterContract, '10000000000000000000').send({ from: account });
-    });
+    document.getElementById("liquidity_approve_token").onclick = submitApproveToken;
+    document.getElementById("submit_add_liquidity").onclick = submitAddLiquidity;
 
     document.getElementById("liquidity_from_token_select").onclick = (() => { openModal('liquidity_from'); });
     document.getElementById("liquidity_to_token_select").onclick = (() => { openModal('liquidity_to'); });
 
     document.getElementById("model_close_btn").onclick = closeModal;
     document.getElementById("btn-login").onclick = login;
-    document.getElementById("from_token_amount").onblur = getQuote;
-    document.getElementById("to_token_amount").onblur = getQuote;
+    document.getElementById("from_token_amount").onblur = getQuoteSwap;
+    document.getElementById("to_token_amount").onblur = getQuoteSwap;
     document.getElementById("custom_token_address").onblur = processCustomToken;
-
+    document.getElementById("submit_swap").onclick = submitSwap;
+    $('#liquidity_approve_token').hide();
+    $('#swap_approve_token').hide();
     login();
 })
-// "[ethjs-query] while formatting outputs from RPC '{"value":{"code":-32603,"data":{"code":-32000,"message":"exceeds block gas limit"}}}'"
